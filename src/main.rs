930f4e49d7799f7cbd96469c881f74c04386180b
Kamil Bednarski <kamilarturbednarski@gmail.com>
2023-03-02 22:38:31 +0100

    feat: add panic if not installed or unable to check

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ mod homebrew;
 fn main() {
     let is_installed = Homebrew::new(Command::new("brew")).check_if_installed();
     match is_installed {
-        Ok(is_installed) => print!("Brew is installed: {}", is_installed),
-        Err(_error) => print!("Error during installation check")
+        Ok(is_installed) => {
+            if !is_installed {
+                panic!("Homebrew not installed");
+            }
+        },
+        Err(error) => panic!("{}", error),
     };
 }
